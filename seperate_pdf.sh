ea9c17aacbd4be23f45524641326b07667ed6a4a
markedphillips <mark.phillips@MEPMBP2017.local>
2019-05-25 14:27:19 -0700

    Notes in seperate

--- seperate_pdf.sh
+++ seperate_pdf.sh
@@ -1,9 +1,14 @@
+# Helper functions and notes
+
+# After generating the mail merge update naming
 # Mail Merge Exhibit Coversheet 1-100
 #rename Ex.pdf to Ex_.pdf
 
-# brew install poppler
+# brew install poppler # for pdfseperate
 # separate each page and number 1-100 with leading zeros for ls sort
 #pdfseparate EX_.pdf EX_%05d.pdf
 
 # Ghostscript
-gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=merged.pdf source1.pdf source2.pdf source3.pdf
+# update opt with letter format
+# look into sizing MB for upload requirements
+#gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=merged.pdf source1.pdf source2.pdf source3.pdf
